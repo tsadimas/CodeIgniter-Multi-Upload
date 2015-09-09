2140c8468cfc816334b185e109f23e779b18cde4
tsadimas <tsadimas@hua.gr>
2015-09-09 18:40:22 +0300

    Update MY_Upload.php

--- MY_Upload.php
+++ MY_Upload.php
@@ -378,7 +378,7 @@
 						}
 						
 						//Sanitize the file name for security.
-						$this->file_name = $this->clean_file_name($this->file_name);
+						$this->file_name = sanitize_filename($this->file_name);
 						
 						//Truncate the file name if it's too long
 						if($this->max_filename > 0){
